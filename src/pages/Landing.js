e58e39bb11ed3e5dc2c072874be05cbef6c357cb
ellenxiao0412@gmail.com <ellenxiao0412@gmail.com>
2022-08-29 15:15:49 +1000

    styling

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,20 +1,47 @@
 import { Link } from "react-router-dom";
+import logoImage from "../assets/logo.png";
 
-const Landing = () => {
+function Landing() {
+    const styles = {
+        ".logo": { height: "70px" },
+        ".background": { backgroundColor: "#F9F5EB" }
+    }
+
     return (
-        <div className="App">
-            <h1>TraceIt - Landing Page</h1>
-            <h2>Group 28</h2>
-            <h3>Pages:</h3>
-            <ul>
-                <li>
-                    <Link to="/login">Login</Link>
-                </li>
-                <li>
-                    <Link to="*">Not Found</Link>
-                </li>
-            </ul>
-        </div>
+        <body class="full-height" style={styles['.background']}>
+            <nav class="navbar">
+                <div class="container">
+                    <div id="navMenu" class="navbar-menu">
+                        <div class="navbar-start">
+                            <a href="/login">
+                                <img class="py-2 mr-6" src={logoImage} style={styles[".logo"]}></img>
+                            </a>
+                            <a class="navbar-item" href="/login">
+                                Login
+                            </a>
+                            <a class="navbar-item" href="/dashboard">
+                                Dashboard
+                            </a>
+                            <a class="navbar-item" href="/*">
+                                Not Found
+                            </a>
+                        </div>
+
+                        <div class="navbar-end">
+                        </div>
+                    </div>
+                </div>
+            </nav>
+
+            <section class="hero is-fullheight-with-navbar">
+                <div class="hero-body">
+                    <p class="title is-size-1">
+                        TraceIt - Group 28
+                    </p>
+                </div>
+            </section>
+        </body>
+
     )
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,46 +1,92 @@
+import 'bulma/css/bulma.min.css';
+import bgImage from "../assets/background.png";
+import logo from "../assets/logo.png";
+// import LoginForm from '../components/LoginForm';
 import React, { useState } from 'react';
-import LoginForm from '../components/LoginForm';
 
 function App() {
-  // hard-coded admin account info
-  const adminUser = {
-    email: "admin@admin.com",
-    password: "admin123"
-  }
 
-  const [user, setUser] = useState({ name: "", email: "" });
-  const [error, setError] = useState("");
-
-  const Login = details => {
-    // console.log(details);
-    if (details.email === adminUser.email && details.password === adminUser.password) {
-      // console.log("Logged in")
-      setUser({
-        name: details.name,
-        email: details.email
-      });
-    }
-    else {
-      // console.log("Details do not match")
-      setError("Details do not match")
-    }
-  }
-  const Logout = () => {
-    setUser({ name: "", email: "" });
-    setError("");
+  const styles = {
+    ".background": { backgroundImage: `url(${bgImage})`, backgroundSize: "cover" },
+    ".loginButton": {
+      position: "absolute",
+      zIndex: 1,
+      backgroundColor: "#1C3879",
+      top: "10px",
+      right: "100px"
+    },
+    ".signupButton": {
+      position: '"absolute"',
+      paddingLeft: "106px",
+      backgroundColor: "#EAE3D2",
+      color: "#607EAA",
+      top: "10px",
+      right: "10px"
+    },
+    ".buttonMarigin": { position: "absolute", top: "30px", right: "50px" },
+    ".box": { margin: "auto" },
+    ".card": {
+      backgroundColor: "#EAE3D2",
+      width: "400px",
+      boxShadow: "0px 109px 337px rgba(0, 0, 0, 0.0815322), 0px 45.5376px 140.791px rgba(0, 0, 0, 0.117129), 0px 24.3466px 75.2734px rgba(0, 0, 0, 0.145), 0px 13.6485px 42.1976px rgba(0, 0, 0, 0.172871), 0px 7.24861px 22.4108px rgba(0, 0, 0, 0.208468), 0px 3.01631px 9.32565px rgba(0, 0, 0, 0.29)",
+      borderRadius: "10px"
+    },
+    ".mainLogo": { width: "200px", margin: "40px auto 0px" },
+    ".welcome": { color: "#F9F5EB" },
+    ".nextButton": { backgroundColor: "#1C3879", },
+    ".field": { borderRadius: "10px" }
   }
 
   return (
-    <div className="App">
-      {(user.email !== "") ? (
-        <div className="Welcome">
-          <h2>Welcome, <span>{user.name}</span></h2>
-          <button onClick={Logout}>Logout</button>
+    <section class="hero is-fullheight" style={styles['.background']}>
+
+      {/* <div class="hero-head" style={converted['.buttonMarigin']}>
+        <button class="button is-primary is-rounded has-text-weight-bold px-5 py-5" style={converted['.loginButton']}>login</button>
+        <button class="button is-link is-rounded has-text-weight-bold py-5" style={converted['.signupButton']}>signup</button>
+      </div> */}
+
+      <div class="hero-body" style={styles['.box']}>
+        <div class="card" style={styles['.card']}>
+          <div class="card-image">
+            <figure class="image" style={styles['.mainLogo']} >
+              <img src={logo} alt="TraceIt Logo" />
+            </figure>
+          </div>
+          <div class="card-content">
+            <div className="is-size-4 has-text-centered has-text-weight-bold pb-5" style={styles['.welcome']}>
+              <p> Welcome Back </p>
+            </div>
+            <div class="content">
+
+              {/* name field */}
+              <div class="field" >
+                <p class="control">
+                  <input class="input" type="text" placeholder="Username" style={styles['.field']} />
+                </p>
+              </div>
+
+              {/* password field */}
+              <div class="field">
+                <p class="control">
+                  <input class="input" type="password" placeholder="Password" style={styles['.field']} />
+                </p>
+              </div>
+
+              {/* Login button */}
+              <div class="field has-text-centered p-5">
+                <p class="control">
+                  <button class="button is-primary is-rounded has-text-weight-bold" style={styles['.nextButton']}>
+                    Login
+                  </button>
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-      ) : (
-        <LoginForm Login={Login} error={error} />
-      )}
-    </div>
+      </div>
+
+    </section >
+
   );
 }
 
